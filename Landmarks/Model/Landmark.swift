8e1ff2f720a41d3aa1dab09447b7b392a1408964
Atikur Rahman <mratikurrahman@gmail.com>
2023-08-20 21:59:43 +0600

    Create landmark model

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,37 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Atikur Rahman on 8/20/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var category: String
+    var city: String
+    var state: String
+    var description: String
+    var isFeatured: Bool
+    var isFavorite: Bool
+    
+    var coordinates: Coordinates
+    var locationCoordicate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude, longitude: coordinates.longitude
+        )
+    }
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+}
