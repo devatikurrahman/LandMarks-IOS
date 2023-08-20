d894484e2cf3216dca163fadd45c2cd697578198
Atikur Rahman <mratikurrahman@gmail.com>
2023-08-21 00:07:34 +0600

    Set Up Navigation List and Detail

---
+++ Landmarks/View/LandmarkDetail.swift
@@ -0,0 +1,54 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Atikur Rahman on 8/20/23.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .padding()
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text here.")
+            }
+        }
+        .padding()
+        
+        Spacer()
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
