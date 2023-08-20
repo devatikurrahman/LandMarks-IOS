def47afa1303b54771c27aa4e635387aac6bb058
Atikur Rahman <mratikurrahman@gmail.com>
2023-08-20 23:38:25 +0600

    Create the List of Landmarks

---
+++ Landmarks/View/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Atikur Rahman on 8/20/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
