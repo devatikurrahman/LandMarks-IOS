c562f9238d1b6aeb54777d35ab2e5f919b85f4fa
Atikur Rahman <mratikurrahman@gmail.com>
2023-08-21 21:51:36 +0600

    Add a Control to Toggle the State

--- Landmarks/View/LandmarkList.swift
+++ Landmarks/View/LandmarkList.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    @State private var showFavoritesOnly = true
+    @State private var showFavoritesOnly = false
     
     var filteredLandmars: [Landmark] {
         landmarks.filter { landmark in
@@ -18,11 +18,17 @@ struct LandmarkList: View {
     
     var body: some View {
         NavigationView {
-            List(filteredLandmars) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                
+                ForEach(filteredLandmars) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
         }
